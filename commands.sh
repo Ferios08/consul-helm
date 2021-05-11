4398503d976c79137a2dc631c965cceb61ca32b1
Firas Chbiki <firas.chbiki@sofrecom.com>
2021-05-12 00:58:50 +0200

    fixed docker image

--- commands.sh
+++ commands.sh
@@ -1,4 +1,4 @@
-docker build --build-arg BUILD_DATE=`date -u +”%Y-%m-%dT%H:%M:%SZ”` \
+cd custom-image && docker build --build-arg BUILD_DATE=`date -u +”%Y-%m-%dT%H:%M:%SZ”` \
              --build-arg ANSIBLE_VERSION="2.9.4"       \
              --build-arg ANSIBLE_LINT_VERSION="5.0.7"   \
              --build-arg MITOGEN_VERSION="0.2.9"         \
@@ -8,6 +8,6 @@ docker push firasdotcom/consul:ansible
 
 ######################################################################
 
-helm upgrade --install consul ./  -f values.yaml  --set global.name=consul
+cd ../ && helm upgrade --install consul ./  -f values.yaml  --set global.name=consul
 
 helm uninstall consul
