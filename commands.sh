954558ef62e645819a6c1beca990d801b21a0f4a
Firas Chbiki <firas.chbiki@sofrecom.com>
2021-05-12 00:48:35 +0200

    added custom docker images

--- commands.sh
+++ commands.sh
@@ -1,2 +1,13 @@
-helm uninstall consul
+docker build --build-arg BUILD_DATE=`date -u +”%Y-%m-%dT%H:%M:%SZ”` \
+             --build-arg ANSIBLE_VERSION="2.9.4"       \
+             --build-arg ANSIBLE_LINT_VERSION="5.0.7"   \
+             --build-arg MITOGEN_VERSION="0.2.9"         \
+             -t firasdotcom/consul:ansible . 
+
+docker push firasdotcom/consul:ansible
+
+######################################################################
+
 helm upgrade --install consul ./  -f values.yaml  --set global.name=consul
+
+helm uninstall consul
